f5b87ded64a61cfad7b1e3f3feabc380e7e8187c
sibelius303 <cesarev17@gmail.com>
2022-10-08 22:53:25 +0000

    Sigue fallando

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,68 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Card } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [ people, setPeople] = useState([]);
+	const [ planets, setPlanets] = useState([]);
+
+	const getPeople = async () => {
+		try {
+			const response = await fetch("https://swapi.dev/api/people");
+			const data = await response.json();
+			setPeople(data.results);
+			console.log(data.results);
+			
+		} catch (error) {
+			console.log(error)
+		}
+	};
+
+	const getPlanets = async () => {
+		try {
+			const response = await fetch("https://swapi.dev/api/planets");
+			const data = await response.json();
+			setPlanets(data.results);
+			console.log(data.results);
+			
+		} catch (error) {
+			console.log(error)
+		}
+	};
+	useEffect(()=>{
+		getPeople();
+		getPlanets();
+	},[]);
+
+
+
+
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div className="container">
+			{
+				people.map((person)=>{
+					return (
+						<Card style={{ width: '18rem' }}> 						
+						<Card.Img variant="top" src="holder.js/100px180" />
+						   <Card.Body>
+							  <Card.Title>{person.name}</Card.Title>
+								 <Card.Text>
+							   <p>climate: {person.hair_color}</p>
+							 <p>graviti: {person.eye_color}</p>
+							 <p>terrain: {person.gender}</p>
+							 </Card.Text>
+							<Button variant="primary">More Details</Button>
+							</Card.Body>
+								 </Card>
+
+					)
+				})
+			}
 		</div>
 	);
 };
